595f0860ca42b4b7eead960156bc228fe949fdaf
Wolinam <zmffhqj35@naver.com>
2021-05-25 04:16:09 +0900

    Add files via upload

---
+++ sketch_may03a.ino
@@ -0,0 +1,61 @@
+int led1 = 8;
+int led2 = 7;
+int led3 = 6;
+int led4 = 5;
+int led5 = 4;
+int led6 = 3;
+
+int sen1 = 9;
+int sen2 = 10;
+int sen3 = 11;
+
+//int button1 = 12;
+int button2 = 13;
+int event = digitalRead(button2);
+int sensor1 = digitalRead(sen1);
+int sensor2 = digitalRead(sen2);
+int sensor3 = digitalRead(sen3);
+
+
+void setup() {
+  for (int i = 3; i < 9; i++) {
+    pinMode(i, OUTPUT);
+  }
+  for (int j = 9; j < 14; j++) {
+    pinMode(j, INPUT);
+  }
+  Serial.begin(9600);
+}
+
+void loop() {
+  //  digitalWrite(button1,LOW);
+  //  digitalWrite(button2,LOW);
+  //  int event = digitalRead(button2);
+
+  if (event == HIGH) {
+    Serial.println("event");
+    for (int i = 3; i <= 8; i++) {
+      digitalWrite(i, LOW);
+      if (sensor1 == 1)
+        digitalWrite(led1, HIGH);
+        Serial.println(sensor1);
+      if (sensor2 == 1)
+        digitalWrite(led2, HIGH);
+      if (sensor3 == 1)
+        digitalWrite(led3, HIGH);
+    }
+    if(led1==HIGH&&led2==HIGH&&led3==HIGH)
+      Serial.println("all");
+  }
+
+  if (event == LOW) {
+    Serial.println("normal");
+    sen1 = 0;
+    sen2 = 0;
+    sen3 = 0;
+    for (int i = 3; i <= 8; i++) {
+      digitalWrite(i, HIGH);
+      Serial.println("light normal");
+    }
+  }
+}
---
+++ sketch_may03b.ino
@@ -0,0 +1,87 @@
+int led1 = 8;
+int led2 = 9;
+int led3 = 10;
+int led4 = 11;
+int led5 = 12;
+int led6 = 13;
+
+int sen1 = A0;
+int sen2 = A1;
+int sen3 = A2;
+
+int sensor1, sensor2, sensor3;
+int distance1, distance2, distance3;
+
+
+void setup() {
+  for (int i = 8; i < 14; i++) {
+    pinMode(i, OUTPUT);
+  }
+
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  sensor1 = map(analogRead(sen1), 0, 1023, 0, 5000);
+  sensor2 = map(analogRead(sen2), 0, 1023, 0, 5000);
+  sensor3 = map(analogRead(sen3), 0, 1023, 0, 5000);
+
+  Serial.print(led1);
+  Serial.print(led2);
+  Serial.print(led3);
+  Serial.print(led4);
+  Serial.print(led5);
+  Serial.print(led6);
+
+
+  distance1 = (27.61 / (sensor1 - 0.1696)) * 1000;
+  Serial.print("distance1 = ");
+  Serial.println(distance1);
+  delay(500);
+  distance2 = (27.61 / (sensor2 - 0.1696)) * 1000;
+  Serial.print("distance2 = ");
+  Serial.println(distance2);
+  delay(500);
+  distance3 = (27.61 / (sensor3 - 0.1696)) * 1000;
+  Serial.print("distance3 = ");
+  Serial.println(distance3);
+  delay(500);
+
+  if (distance1 < 25)
+    digitalWrite(led1, HIGH);
+  else
+    digitalWrite(led1, LOW);
+  if (distance2 < 25)
+    digitalWrite(led2, HIGH);
+  else
+    digitalWrite(led2, LOW);
+  if (distance3 < 25)
+    digitalWrite(led3, HIGH);
+  else
+    digitalWrite(led3, LOW);
+
+  char a = Serial.read();
+  switch (a) {
+    case 'b':
+      sensor1 = 0;
+      sensor2 = 0;
+      sensor3 = 0;
+      for (int i = 8; i <= 13; i++) {
+        digitalWrite(i, HIGH);
+        Serial.println("see");
+      }
+    case 'c':
+      sensor1 = 0;
+      sensor2 = 0;
+      sensor3 = 0;
+      for (int i = 8; i <= 13; i++) {
+        digitalWrite(i, LOW);
+        Serial.println("off");
+      }
+  }
+}
